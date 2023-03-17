1b65773a5aae48259ef2fc72c50f1827009db518
Abdelrahman <abdo.moustafa.1998@gmail.com>
2023-03-18 01:16:41 +0200

    Initial Commit

---
+++ AssisAi/AssisAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssisAiApp.swift
+//  AssisAi
+//
+//  Created by Abdelrahman Moustafa on 18/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AssisAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
